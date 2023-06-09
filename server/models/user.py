6b3265b8f852dc75044cbdc51d9ba4e76f84c6d8
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-06-09 10:41:31 +0400

    Update user.py
    correct my typo (nick length should not be 87)

--- server/models/user.py
+++ server/models/user.py
@@ -5,7 +5,7 @@ class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(87), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(87), nullable=False)
 
     def json(self):
