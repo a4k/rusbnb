265712400b93f25ef172de9ad2099481f7abe5c5
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-06-11 15:11:20 +0400

    update room.py
    updated import in `server/resources/room.py`

--- server/resources/room.py
+++ server/resources/room.py
@@ -1,11 +1,12 @@
 from flask_restful import Resource
+from sqlalchemy.exc import SQLAlchemyError
 from models import RoomModel
 
 
 class RoomList(Resource):
     def get(self):
         data = {"rooms": []}
-        raw_data = roomModel.find_all()
+        raw_data = RoomModel.find_all()
         for obj in raw_data:
             data['rooms'].append(obj.json())
         return data, 200
