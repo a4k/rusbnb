083803f7033ef8381d29bf1f8528a1af84af05dd
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-06-14 11:01:50 +0400

    Create room-photo.py
    created database model of room photos

---
+++ server/models/room-photo.py
@@ -0,0 +1,33 @@
+from db import db
+
+
+class RoomModel(db.Model):
+    __tablename__ = 'Room-photo'
+
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.Integer, nullable=False)
+    format = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    
+
+    def json(self):
+        return {
+            'id': self.id,
+            'room_id': self.room_id,
+            'title': self.title,
+            'description': self.description,
+            'format': self.format
+        }
+
+    @classmethod
+    def find_by_room_id(cls, room_id):
+        return cls.query.filter_by(id=room_id).all()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
