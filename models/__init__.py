b36d38b86d45267887da409f78c2af654b6eb526
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-06-15 14:51:48 +0400

    Большой глобальный апдейт с исправлением ошибок
    исправлена опечатка в room.py:
    28: _rooms_post_parser = reqparse.RequestParser()
    добавлен обьект RequestParser'a
    
    исправлена опечатка в room_photo в GET запросе:
    18: raw_data = RoomModel.find_by_room_id(room_id)
    (значение room_id получается не из тела пост запроса а из ссылки)
    
    файл models/room-photo.py переименован в models/room_photo.py
    (чтобы не вызывать ошибки в коде питона который воспринимает - как оператор вычитания)
    
    обновлен файл models/__init__.py:
    4: from models.room_photo import RoomPhotoModel
    добавлен импорт модели фото для правильного импорта модели в файле resources/room_photo.py
    
    изменен файл resources/room_photo.py:
    ~ GET запрос. добавлена проверка наличия фотографий
    ~ POST запрос. переписан парсер. Доделано сохранение фотографии в необходимом формате.
    
    вцелом исправлены ошибки, проведен запуск.

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from models.user import UserModel
+from models.store import StoreModel
+from models.room import RoomModel
+from models.room_photo import RoomPhotoModel
---
+++ resources/room.py
@@ -0,0 +1,73 @@
+from flask_restful import Resource, reqparse
+from sqlalchemy.exc import SQLAlchemyError
+from models import RoomModel
+
+_rooms_pagination_parser = reqparse.RequestParser()
+_rooms_pagination_parser.add_argument(
+    "offset", 
+    type=int, 
+    required=False,
+    default="0", 
+    help="This field show how many rooms to skip"
+)
+_rooms_pagination_parser.add_argument(
+    "size", 
+    type=str, 
+    required=False, 
+    default="50",
+    help="This field shows how many rooms to get per request"
+)
+_rooms_pagination_parser.add_argument(
+    "sort_by_cost", 
+    type=bool, 
+    required=False, 
+    default=False,
+    help="This field shows, is it necessary to sort by price of Rooms or not"
+)
+
+_rooms_post_parser = reqparse.RequestParser()
+_rooms_post_parser.add_argument(
+    "title", type=str, required=True, help = "this field is a title of room"
+)
+_rooms_post_parser.add_argument(
+    "subtitle", type=str, required=True, help = "this field is a subtitle of room"
+)
+_rooms_post_parser.add_argument(
+    "description", type=str, required=True, help = "this field is a description about room"
+)
+_rooms_post_parser.add_argument(
+    "price", type=int, required=True, help = "this field is a title of room"
+)
+
+class Rooms(Resource):
+    def get(self):
+        req_data = _rooms_pagination_parser.parse_args()
+
+        if req_data['size'] == 'all':
+            raw_data = RoomModel.find_all(sort_by_cost=bool(req_data['sort_by_cost']))
+        else:
+            raw_data = RoomModel.find_list(
+                req_data['offset'], 
+                req_data['size'],
+                sort_by_cost=bool(req_data['sort_by_cost'])
+            )
+
+        data = {"rooms": []}
+
+        for obj in raw_data:
+            data['rooms'].append(obj.json())
+
+        return data, 200
+
+    def post(self):
+        req_data = _rooms_post_parser.parse_args()
+
+        room = RoomModel(
+            title=req_data['title'],
+            subtitle=req_data['subtitle'],
+            description=req_data['description'],
+            price=req_data['price'],
+            rate=5.0
+        )
+        room.save_to_db()
+        return 200
