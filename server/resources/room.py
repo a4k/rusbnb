b913bde5c7602a170d36098eee8d8f3cf805acda
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-06-23 17:47:23 +0400

    Revert "починил едпоинт"

--- server/resources/room.py
+++ server/resources/room.py
@@ -1,8 +1,29 @@
 from flask_restful import Resource, reqparse
 from sqlalchemy.exc import SQLAlchemyError
 from models import RoomModel
-from flask import request
 
+_rooms_pagination_parser = reqparse.RequestParser()
+_rooms_pagination_parser.add_argument(
+    "offset", 
+    type=int, 
+    required=False,
+    default="0", 
+    help="This field show how many rooms to skip"
+)
+_rooms_pagination_parser.add_argument(
+    "size", 
+    type=str, 
+    required=False, 
+    default="50",
+    help="This field shows how many rooms to get per request"
+)
+_rooms_pagination_parser.add_argument(
+    "sort_by_cost", 
+    type=bool, 
+    required=False, 
+    default=False,
+    help="This field shows, is it necessary to sort by price of Rooms or not"
+)
 
 _rooms_post_parser = reqparse.RequestParser()
 _rooms_post_parser.add_argument(
@@ -21,7 +42,7 @@ _rooms_post_parser.add_argument(
 
 class Rooms(Resource):
     def get(self):
-        req_data = request.args
+        req_data = _rooms_pagination_parser.parse_args()
 
         if req_data['size'] == 'all':
             raw_data = RoomModel.find_all(sort_by_cost=bool(req_data['sort_by_cost']))
