52533e5d724e4c13041a52308dc2a99df287172c
ki7883521 <98214975+ki7883521@users.noreply.github.com>
2023-06-25 17:21:40 +0400

    Создание post запроса для загрузки аватарки

--- server/resources/user.py
+++ server/resources/user.py
@@ -1,7 +1,22 @@
 from flask_restful import Resource, reqparse
 from passlib.hash import pbkdf2_sha256
-
+from db import db
 from models import UserModel
+from PIL import Image
+from flask import request
+from http import HTTPStatus
+
+
+def get_extension_from_filename(filename: str):
+    return filename.split(".")[-1]
+
+
+def handle_extension(current_extension: str, allowed_extensions: list) -> str:
+    if current_extension not in allowed_extensions:
+        extension = allowed_extensions[0]
+        return extension
+    return current_extension
+
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument(
@@ -80,3 +95,17 @@ class User(Resource):
             return {"message": "User Not Found"}, 404
         user.delete_from_db()
         return {"message": "User deleted."}, 200
+
+
+class Add_avatar(Resource):
+    def post(self, user_id):
+        photo_file = request.files['photo']
+        user = UserModel.find_by_id(user_id)
+        user.name_image = str(user_id) + '.png'
+        db.session.commit()
+
+        with Image.open(photo_file) as photo_image:
+            photo_image.save(f'User_avatars/{user.name_image}')
+
+        return {"message": "Photo successfully uploaded"}, HTTPStatus.ACCEPTED
+
