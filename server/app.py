948c113298d65443815e0f065ad0e1a69dbd397a
nbrovkovich <brovkovich@dev.vtb.ru>
2023-06-28 12:40:23 +0400

    Добавлена библиотека environ

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_file
 from flask_restful import Api
+from os import environ
+
 from flask_cors import CORS # Cross Origin Response Control
 from db import db
 from resources import *
