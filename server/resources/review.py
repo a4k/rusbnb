c6994fea61b3399cf1630232049e622751902cc0
exp-of-betrayal <none@null.0>
2023-06-28 23:02:03 +0400

    add resource Reviews

---
+++ server/resources/review.py
@@ -0,0 +1,62 @@
+from flask_restful import Resource, reqparse
+from http import HTTPStatus
+from models import ReviewModel
+
+
+review_object_parser = reqparse.RequestParser()
+review_object_parser.add_argument(
+    "user_id", type=int, required=True
+)
+review_object_parser.add_argument(
+    "review_text", type=str, required=True
+)
+review_object_parser.add_argument(
+    "rate", type=float, required=True
+)
+
+review_put_object_parser = reqparse.RequestParser()
+review_put_object_parser.add_argument(
+    "review_text", type=str, required=True
+)
+review_put_object_parser.add_argument(
+    "rate", type=float, required=True
+)
+
+
+class Reviews(Resource):
+    # /reviews
+
+    @classmethod
+    def get(cls, room_id: int):
+        room_review_list = ReviewModel.find_by_room_id(room_id)
+        if not room_review_list:
+            return {"message": "reviews not found"}, HTTPStatus.NOT_FOUND
+        json_response = {"reviews": [review.json for review in room_review_list]}, HTTPStatus.OK
+        return json_response
+
+    @classmethod
+    def post(cls):
+        request_args = review_object_parser.parse_args()
+
+        review = ReviewModel(
+            user_id=request_args["user_id"],
+            review_text=request_args["review_text"],
+            rate=request_args["rate"]
+        )
+        review.save_to_db()
+        return {"message": "Successfully created review"}, HTTPStatus.ACCEPTED
+
+    @classmethod
+    def put(cls, review_id: int):
+        request_args = review_put_object_parser.parse_args()
+        review = ReviewModel.find_by_id(review_id)
+        review.review_text = request_args["review_text"]
+        review.rate = request_args["rate"]
+        return {"message": "Successfully modify review"}, HTTPStatus.ACCEPTED
+
+
+    @classmethod
+    def delete(cls, review_id: int):
+        review = ReviewModel.find_by_id(review_id)
+        review.delete_from_bd()
+        return {"message": "Successfully delete review"}, HTTPStatus.OK
