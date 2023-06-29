54dd8035f5fa722f9a50e77db0c9bfa188457ff0
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-06-29 11:24:56 +0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -15,7 +15,7 @@ from db import db
 app = Flask(__name__)
 
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres_database:GEm6uKZkmmDOyWRyDdhFBPiflcG2Ckqb@dpg-cieitotgkuvlk1g90fpg-a.frankfurt-postgres.render.com/postgres_database_f37x"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres_database:GEm6uKZkmmDOyWRyDdhFBPiflcG2Ckqb@dpg-cieitotgkuvlk1g90fpg-a.frankfurt-postgres.render.com/postgres_database_f37x"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 db.init_app(app)
