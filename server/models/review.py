1a66806a27f182c5f41ad78b144cf0e091d6aaf0
exp-of-betrayal <glebisi@yandex.ru>
2023-07-20 12:35:40 +0400

    во!

--- server/models/review.py
+++ server/models/review.py
@@ -24,7 +24,7 @@ class ReviewModel(db.Model):
 
     @classmethod
     def average_rate_by_id(cls, room_id):
-        reviews_list = cls.query.filter_by(cls.room_id == room_id).all()
+        reviews_list = cls.query.filter_by(cls.room_id = room_id).all()
         rate_list = [review.rate for review in reviews_list]
         if rate_list:
             return sum(rate_list) / len(rate_list)
@@ -32,7 +32,7 @@ class ReviewModel(db.Model):
 
     @classmethod
     def find_by_room_id(cls, room_id):
-        return cls.query.filter_by(cls.room_id == room_id).all()
+        return cls.query.filter_by(cls.room_id = room_id).all()
 
     def save_to_db(self):
         db.session.add(self)
